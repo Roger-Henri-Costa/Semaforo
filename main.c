6990b058f526454041ba78b1eb49e43edf5871e0
Roger <roger7costa@gmail.com>
2025-04-10 09:32:06 -0300

    atualização de arquivos

--- main.c
+++ main.c
@@ -1,46 +1,32 @@
 #define F_CPU 16000000L
 #include <avr/io.h>
 #include <util/delay.h>
+#define VAI_CARRO 0x88
+#define VAI_PED 0x60
+#define ATENCAO_CARRO 0x90
+#define PISCA_PED_VM 0xA0
+#define PISCA_PED 0x20
+
+
 int main(void) 
 {   
     //ENTRADAS 
-	DDRB &= ~(1<<0);
+	DDRB &= ~0x01;
 	
-    PORTB |= (1<<0);
+    PORTB |= 0x01;
     
 	// SAÍDAS
-	DDRD |= (1<<3); // Sv_Vd
-	DDRD |= (1<<4); // Sv_Am
-	DDRD |= (1<<5); // Sv_Vm
-	DDRD |= (1<<6); // Pd_Vd
-	DDRD |= (1<<7); // Pd_Vm
+	DDRD |= 0xF8;
 	
-	PORTD &= ~(1<<3); // Sv_Vd
-	PORTD &= ~(1<<4); // Sv_Am
-	PORTD &= ~(1<<5); // Sv_Vm
-	PORTD &= ~(1<<6); // Pd_Vd
-	PORTD &= ~(1<<7); // Pd_Vm
+	PORTD &= ~0xF8;
 	
 	int estado = 0;
 	int tempo = 0;
 	while (1)
     {
-		/*if (!( PINB & (1<<0)))
-		{
-			PORTD |= (1<<3);
-			PORTD |= (1<<4);
-			PORTD |= (1<<5);
-			PORTD |= (1<<6);
-			PORTD |= (1<<7);*/
-		
-			
 		if (estado == 0)
 		{
-			PORTD &= ~(1<<3); // Sv_Vd
-			PORTD &= ~(1<<4); // Sv_Am
-			PORTD |= (1<<5); // Sv_Vm
-			PORTD &= ~(1<<7); // Pd_Vm
-			PORTD |= (1<<6); // Pd_Vd
+			PORTD = VAI_PED;
 			tempo = 15000;
 			estado = 1;
 		}
@@ -50,24 +36,23 @@ int main(void)
 			_delay_ms(1);
 			if (tempo == 5000)
 			{
-			PORTD |= (1<<7); // Pd_Vm
-			PORTD &= ~(1<<6); // Pd_Vd
+			PORTD = PISCA_PED_VM;
 			}
 			if (tempo == 4000)
 			{
-			PORTD &= ~(1<<7); // Pd_Vm
+			PORTD = PISCA_PED;
 			}
 			if (tempo == 3000)
 			{
-			PORTD |= (1<<7); // Pd_Vm
+			PORTD = PISCA_PED_VM;
 			}
 			if (tempo == 2000)
 			{
-			PORTD &= ~(1<<7); // Pd_Vm
+			PORTD = PISCA_PED;
 			}
 			if (tempo == 1000)
 			{
-			PORTD |= (1<<7); // Pd_Vm
+			PORTD = PISCA_PED_VM;
 			}
 			if (tempo == 0)
 			{
@@ -76,10 +61,7 @@ int main(void)
 		}
 		if (estado == 2)
 		{
-			PORTD &= ~(1<<5); // Sv_Vm
-			PORTD |= (1<<3); // Sv_Vd
-			PORTD &= ~(1<<6); // Pd_Vd
-			PORTD |= (1<<7); // Pd_Vm
+			PORTD = VAI_CARRO;
 			tempo = 42000;
 			estado = 3;
 		}
@@ -94,8 +76,7 @@ int main(void)
 		}
 		if (estado == 4)
 		{
-			PORTD &= ~(1<<3); // Sv_Vd
-			PORTD |= (1<<4); // Sv_Am
+			PORTD = ATENCAO_CARRO;
 			tempo = 3000;
 			estado = 5;
 		}
@@ -115,48 +96,5 @@ int main(void)
 				estado = 4;
 			}
 		}
-
-
-
-		
-		/*	if (!( PINB & (1<<0)))
-			{
-				estado = 1;
-			}
-		}
-		if (estado ==1)
-		{
-			PORTD |= (1<<5);
-			PORTD |= (1<<6);
-			tempo = 5000;
-			estado = 2;
-		}
-		if (estado == 2)
-		{
-			--tempo;
-			_delay_ms(1);
-			if (tempo == 0)
-			{
-				estado = 3;
-			}
-		}
-		if (estado == 3)
-		{
-			PORTD &= ~(1<<6);
-			PORTD |= (1<<7);
-			estado = 4;
-		}
-		if (estado == 4)
-		{
-			if (!( PINB & (1<<1)))
-			{
-				estado = 0;
-			}
-		}
-		if ((!( PINB & (1<<1)))&& (estado ==2))
-		{
-			estado = 0;
-		}*/
-		
     }
 }
